0a98a76de33b3b8f192e755f464c86cdeaac064f
Caio Guine Pascoal <49839590+caiopascoal@users.noreply.github.com>
2020-12-05 23:16:16 -0300

    Update produces.py

--- produces.py
+++ produces.py
@@ -1,4 +1,4 @@
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0,10))
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
 producer.send('quickstart-events', b'a')
 #producer.flush()
