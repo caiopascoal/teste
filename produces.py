f44cb2e1360d214c53482d79e4d0e2f83f35059f
Caio Guine Pascoal <49839590+caiopascoal@users.noreply.github.com>
2020-12-05 23:57:05 -0300

    Update produces.py

--- produces.py
+++ produces.py
@@ -1,12 +1,16 @@
 from kafka import KafkaProducer
 from json import dumps, loads, load
+import pandas as pd
 
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda
    K:dumps(K).encode('utf-8'))
+total = 0
 
-with open('/home/ec2-user/test.json') as file:
-    reader = load(file)
-    for msg in reader:
+for chunk in pd.read_json('/home/ec2-user/test.json', chunksize=1000):
+    print(chunk)
+    total += sum(chunk['x'])
+    for msg in chunk:
+        print(msg)
         producer.send('quickstart-events',msg)
         producer.flush()
