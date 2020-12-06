ecf595d87ba8bb00733e52428684e24d1981f4be
Caio Guine Pascoal <49839590+caiopascoal@users.noreply.github.com>
2020-12-06 00:02:36 -0300

    Update produces.py

--- produces.py
+++ produces.py
@@ -10,7 +10,8 @@ total = 0
 for chunk in pd.read_json('/home/ec2-user/test.json', chunksize=1000, lines=True):
     print(chunk)
     total += sum(chunk['x'])
-    for msg in chunk:
-        print(msg)
-        producer.send('quickstart-events',msg)
-        producer.flush()
+#    for msg in chunk:
+#        print(msg)
+#        producer.send('quickstart-events',msg)
+#        producer.flush()
+print(total)
