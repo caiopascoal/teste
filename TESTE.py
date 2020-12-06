331bf7642eeee8115be13369c0b5103656cfa3ad
Caio Guine Pascoal <49839590+caiopascoal@users.noreply.github.com>
2020-12-05 23:22:23 -0300

    Update TESTE.py

--- TESTE.py
+++ TESTE.py
@@ -7,4 +7,4 @@
 from kafka import KafkaConsumer
 consumer = KafkaConsumer('quickstart-events', bootstrap_servers=['localhost:9092'], api_version=(0,10))
 for msg in consumer:
-    print("Topic    Name=%s,Message=%s" % (msg.topic, msg.value))
+    print(msg) 
