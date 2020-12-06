702b90ad2faae5fa077e681ac42615ec39cb03d7
Caio Guine Pascoal <49839590+caiopascoal@users.noreply.github.com>
2020-12-05 23:46:48 -0300

    Update produces.py

--- produces.py
+++ produces.py
@@ -1,6 +1,12 @@
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+from json import dumps, loads, load
+
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda
+   K:dumps(K).encode('utf-8'))
 
 with open('/home/ec2-user/test.json') as file:
-    producer.send('quickstart-events', file.read())
-    producer.flush()
+    reader = load(file)
+    for msg in reader:
+        producer.send('quickstart-events',msg)
+        producer.flush()
