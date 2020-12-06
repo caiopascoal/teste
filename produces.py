a068e8d1e0d9dd5a05f8282c05a0581cd7e56b71
Caio Guine Pascoal <49839590+caiopascoal@users.noreply.github.com>
2020-12-06 00:09:30 -0300

    Update produces.py

--- produces.py
+++ produces.py
@@ -9,7 +9,14 @@ file_name = '/home/ec2-user/test.json'
 
 print(f'File Size is {os.stat(file_name).st_size / (1024 * 1024)} MB')
 
+txt_file = open(file_name)
+count = 0
+for line in txt_file:
+    # we can process file line by line here, for simplicity I am taking count of lines
+    count += 1
+txt_file.close()
 
+print(f'Number of Lines in the file is {count}')
 #producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda
 #   K:dumps(K).encode('utf-8'))
 #total = 0
