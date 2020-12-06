2eee20d64c430148679a38e0e21f55cce27d4196
Caio Guine Pascoal <49839590+caiopascoal@users.noreply.github.com>
2020-12-05 23:39:22 -0300

    Update produces.py

--- produces.py
+++ produces.py
@@ -1,6 +1,5 @@
 from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-with open('/home/ec2-user/test.json') as file:
-    producer.send('quickstart-events', file.flush())
-    producer.flush()
+producer.send('quickstart-events','/home/ec2-user/test.json')
+producer.flush()
